520b77a1307424fa2601c7582a3a8477a7a07767
TorionJJ <timur-will@yandex.ru>
2020-04-15 04:13:17 +0300

    BD

--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -10,10 +10,8 @@ public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-
     private String text;
     private String tag;
-
     public Message() {
     }
 
